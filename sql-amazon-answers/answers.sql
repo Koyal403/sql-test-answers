5dc55a8aaaa2846f21521883264258d6883ae351
Koyal Roy <koyel.roy6191@gmail.com>
2020-11-04 01:01:58 +0530

    first sample query

--- sql-amazon-answers/answers.sql
+++ sql-amazon-answers/answers.sql
@@ -1,9 +1,11 @@
-CREATE TABLE Persons (
+CREATE TABLE Amazon.Persons (
     PersonID int,
     LastName varchar(255),
     FirstName varchar(255),
     Address varchar(255),
     City varchar(255),
-    PhoneNumber int
+    PhoneNumber INT
 
 );
+
+INSERT INTO Amazon.Persons (PersonID, LastName,FirstName) values (1,'Roy','Koyal')
